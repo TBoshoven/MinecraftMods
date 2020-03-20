31fbc6306894716402d0bc10e76903430f93a2ef
Tom Boshoven <tomboshoven@gmail.com>
2020-03-20 19:47:02 -0400

    Completely change model loading. The new model system is pretty great. I still have to translate the models to the new system.

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/ModelLoaders.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/ModelLoaders.java
@@ -3,7 +3,7 @@ package com.tomboshoven.minecraft.magicdoorknob.modelloaders;
 import com.tomboshoven.minecraft.magicdoorknob.modelloaders.textured.TexturedModelLoader;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.model.ModelLoaderRegistry2;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -18,24 +18,11 @@ public final class ModelLoaders {
     private ModelLoaders() {
     }
 
-    private static String[] TEXTURED_MODELS = new String[] {
-            "magic_door", "magic_door_knob", "magic_doorway_pillar", "magic_doorway_pillar_short", "magic_doorway_top",
-            "magic_doorway_wall", "magic_doorway_wall_narrow", "magic_doorway_wall_narrow_short",
-            "magic_doorway_wall_short",
-    };
-
     @SubscribeEvent
     public static void registerModelLoaders(ModelRegistryEvent event) {
         // Initialize textured model loader
         TexturedModelLoader modelLoader = new TexturedModelLoader();
-        // Register all textured models with the model loader
-        for (String name : TEXTURED_MODELS) {
-            modelLoader.register(
-                    new ResourceLocation(MOD_ID, "block/" + name),
-                    new ResourceLocation(MOD_ID, "block/textured/" + name)
-            );
-        }
-        modelLoader.registerTexture(new ResourceLocation("magic_doorknob", "block/empty"));
-        ModelLoaderRegistry.registerLoader(modelLoader);
+        modelLoader.registerTexture(new ResourceLocation(MOD_ID, "block/empty"));
+        ModelLoaderRegistry2.registerLoader(new ResourceLocation(MOD_ID, "textured"), modelLoader);
     }
 }
--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/textured/TexturedModelLoader.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/textured/TexturedModelLoader.java
@@ -1,20 +1,18 @@
 package com.tomboshoven.minecraft.magicdoorknob.modelloaders.textured;
 
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.client.renderer.model.IUnbakedModel;
-import net.minecraft.client.renderer.model.ModelBakery;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ICustomModelLoader;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.model.IModelLoader;
+import net.minecraftforge.client.model.ModelLoaderRegistry2;
+import net.minecraftforge.client.model.geometry.IModelGeometry;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -23,33 +21,14 @@ import java.util.Set;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @OnlyIn(Dist.CLIENT)
-public class TexturedModelLoader implements ICustomModelLoader {
+public class TexturedModelLoader implements IModelLoader<TexturedModelGeometry> {
     // The namespace of the properties; used in model definitions
     public static final String PROPERTY_NAMESPACE = "property";
-    // A mapping from configured textured models to the models they're based on
-    private Map<ResourceLocation, ResourceLocation> baseModels = Maps.newHashMap();
     // All extra textures that were requested.
     private Set<ResourceLocation> extraTextures = Sets.newHashSet();
 
     @Override
     public void onResourceManagerReload(IResourceManager resourceManager) {
-    }
-
-    @Override
-    public boolean accepts(ResourceLocation modelLocation) {
-        // Check to see if we know about the given model
-        return baseModels.containsKey(new ResourceLocation(modelLocation.getNamespace(), modelLocation.getPath()));
-    }
-
-    /**
-     * Register a new textured model.
-     *
-     * @param modelLocation     The location for the textured model
-     * @param baseModelLocation The location of the model to base it on
-     */
-    public void register(ResourceLocation modelLocation, ResourceLocation baseModelLocation) {
-        ResourceLocation actualLocation = ModelLoaderRegistry.getActualLocation(modelLocation);
-        baseModels.put(actualLocation, baseModelLocation);
     }
 
     /**
@@ -63,10 +42,10 @@ public class TexturedModelLoader implements ICustomModelLoader {
     }
 
     @Override
-    public IUnbakedModel loadModel(ResourceLocation modelLocation) throws Exception {
-        ResourceLocation baseModelLocation = baseModels.getOrDefault(modelLocation, ModelBakery.MODEL_MISSING);
-        // FIXME: This causes the untextured model to be cached and dummy textures to be requested
-        IUnbakedModel baseModel = ModelLoaderRegistry.getModel(baseModelLocation);
-        return new TexturedModel(baseModel, Collections.unmodifiableSet(extraTextures));
+    public TexturedModelGeometry read(JsonDeserializationContext deserializationContext, JsonObject modelContents) {
+        // Load the original model through its configured base loader
+        ResourceLocation baseLoader = new ResourceLocation(modelContents.get("base_loader").getAsString());
+        IModelGeometry<?> modelGeometry = ModelLoaderRegistry2.getModel(baseLoader, deserializationContext, modelContents);
+        return new TexturedModelGeometry(modelGeometry, extraTextures);
     }
 }
