e9892d210cb03936a7a1d48d39dfbfead2a025b7
Tom Boshoven <tomboshoven@gmail.com>
2020-03-20 19:27:20 -0400

    Simplify baked model wrapping by using an existing wrapper class.

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/textured/TexturedBakedModel.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/textured/TexturedBakedModel.java
@@ -6,7 +6,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.model.BakedQuad;
 import net.minecraft.client.renderer.model.BakedQuadRetextured;
 import net.minecraft.client.renderer.model.IBakedModel;
-import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.model.ItemOverride;
 import net.minecraft.client.renderer.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -16,15 +15,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.BakedModelWrapper;
 import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.client.model.data.IModelData;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import javax.vecmath.Matrix4f;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
@@ -36,21 +34,19 @@ import java.util.stream.Collectors;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @OnlyIn(Dist.CLIENT)
-class TexturedBakedModel implements IBakedModel {
-    // The original baked model
-    private IBakedModel wrappedBakedModel;
+class TexturedBakedModel<T extends IBakedModel> extends BakedModelWrapper<T> {
     // The baked texture getter
     private Function<? super ResourceLocation, ? extends TextureAtlasSprite> bakedTextureGetter;
     // The mapper that replaces property textures by their values
     private ITextureMapper textureMapper;
 
     /**
-     * @param wrappedBakedModel  The original baked model
+     * @param originalModel      The original baked model
      * @param bakedTextureGetter The baked texture getter
      * @param textureMapper      The mapper that replaces property textures by their values
      */
-    TexturedBakedModel(IBakedModel wrappedBakedModel, Function<? super ResourceLocation, ? extends TextureAtlasSprite> bakedTextureGetter, ITextureMapper textureMapper) {
-        this.wrappedBakedModel = wrappedBakedModel;
+    TexturedBakedModel(T originalModel, Function<? super ResourceLocation, ? extends TextureAtlasSprite> bakedTextureGetter, ITextureMapper textureMapper) {
+        super(originalModel);
         this.bakedTextureGetter = bakedTextureGetter;
         this.textureMapper = textureMapper;
     }
@@ -58,7 +54,7 @@ class TexturedBakedModel implements IBakedModel {
     @Override
     public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, Random rand, IModelData extraData) {
         // Return the original quads, with the property sprites replaced by actual ones
-        List<BakedQuad> quads = wrappedBakedModel.getQuads(state, side, rand, extraData);
+        List<BakedQuad> quads = originalModel.getQuads(state, side, rand, extraData);
         return quads.stream().map(quad -> {
             TextureAtlasSprite sprite = quad.getSprite();
             if (sprite instanceof PropertySprite) {
@@ -76,46 +72,8 @@ class TexturedBakedModel implements IBakedModel {
     }
 
     @Override
-    public boolean isAmbientOcclusion() {
-        return wrappedBakedModel.isAmbientOcclusion();
-    }
-
-    @Override
-    public boolean isGui3d() {
-        return wrappedBakedModel.isGui3d();
-    }
-
-    @Override
-    public boolean isBuiltInRenderer() {
-        return wrappedBakedModel.isBuiltInRenderer();
-    }
-
-    @Override
-    public TextureAtlasSprite getParticleTexture() {
-        return wrappedBakedModel.getParticleTexture();
-    }
-
-    @Override
-    public ItemCameraTransforms getItemCameraTransforms() {
-        return wrappedBakedModel.getItemCameraTransforms();
-    }
-
-    @Override
     public ItemOverrideList getOverrides() {
-        return new TexturedOverrideList(wrappedBakedModel.getOverrides());
-    }
-
-    @Override
-    public boolean isAmbientOcclusion(BlockState state) {
-        return wrappedBakedModel.isAmbientOcclusion();
-    }
-
-    @Override
-    public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType) {
-        // No easy way to fix the first element of the tuple, but it's very rarely different from the original baked
-        // model, so we just replace it by this.
-        Pair<? extends IBakedModel, Matrix4f> result = wrappedBakedModel.handlePerspective(cameraTransformType);
-        return Pair.of(this, result.getRight());
+        return new TexturedOverrideList(super.getOverrides());
     }
 
     /**
@@ -142,7 +100,7 @@ class TexturedBakedModel implements IBakedModel {
             // If the item has a texture mapper, use it.
             Item item = stack.getItem();
             if (item instanceof IItemStackTextureMapperProvider) {
-                return new TexturedBakedModel(wrappedBakedModel, bakedTextureGetter, ((IItemStackTextureMapperProvider) item).getTextureMapper(stack));
+                return new TexturedBakedModel<>(originalModel, bakedTextureGetter, ((IItemStackTextureMapperProvider) item).getTextureMapper(stack));
             }
             return wrappedOverrideList.getModelWithOverrides(originalModel, stack, world, entity);
         }
