b6384543333835575736700a7671f34c9bb752a2
Tom Boshoven <tomboshoven@gmail.com>
2020-05-30 13:06:19 -0400

    Data generator for doorknob item models.

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/data/DataGenerators.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/data/DataGenerators.java
@@ -2,6 +2,7 @@ package com.tomboshoven.minecraft.magicdoorknob.data;
 
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
@@ -13,6 +14,13 @@ public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
-        generator.addProvider(new Recipes(generator));
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        if (event.includeServer()) {
+            generator.addProvider(new Recipes(generator));
+        }
+        if (event.includeClient()) {
+            generator.addProvider(new ItemModels(generator, existingFileHelper));
+        }
     }
 }
