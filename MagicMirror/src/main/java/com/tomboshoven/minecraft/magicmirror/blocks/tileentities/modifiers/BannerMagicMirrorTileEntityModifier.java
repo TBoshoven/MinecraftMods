4ae969b859127c83ed91efb98f926e2dea6f4d6b
Tom Boshoven <tomboshoven@gmail.com>
2020-06-20 13:58:08 -0400

    Get the pattern list from the banner NBT and pass it to the reflection modifier.

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/tileentities/modifiers/BannerMagicMirrorTileEntityModifier.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/tileentities/modifiers/BannerMagicMirrorTileEntityModifier.java
@@ -1,5 +1,6 @@
 package com.tomboshoven.minecraft.magicmirror.blocks.tileentities.modifiers;
 
+import com.google.common.collect.Lists;
 import com.tomboshoven.minecraft.magicmirror.blocks.modifiers.MagicMirrorModifier;
 import com.tomboshoven.minecraft.magicmirror.blocks.tileentities.MagicMirrorBaseTileEntity;
 import com.tomboshoven.minecraft.magicmirror.reflection.Reflection;
@@ -12,14 +13,18 @@ import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.tileentity.BannerPattern;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.DistExecutor;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 
 @ParametersAreNonnullByDefault
@@ -94,15 +99,32 @@ public class BannerMagicMirrorTileEntityModifier extends MagicMirrorTileEntityMo
     public void activate(MagicMirrorBaseTileEntity tileEntity) {
         Reflection reflection = tileEntity.getReflection();
         if (reflection != null) {
-            reflectionModifier = createReflectionModifier();
+            List<Pair<BannerPattern, DyeColor>> patternList = Lists.newArrayList();
+            patternList.add(Pair.of(BannerPattern.BASE, baseColor));
+            if (bannerNBT != null) {
+                // Get the patterns from the NBT data
+                ListNBT patterns = bannerNBT.getList("Patterns", 10);
+                int size = patterns.size();
+                for (int i = 0; i < size; ++i) {
+                    CompoundNBT pattern = patterns.getCompound(i);
+                    BannerPattern bannerPattern = BannerPattern.byHash(pattern.getString("Pattern"));
+                    if (bannerPattern != null) {
+                        DyeColor bannerPatternColor = DyeColor.byId(pattern.getInt("Color"));
+                        patternList.add(Pair.of(bannerPattern, bannerPatternColor));
+                    }
+                }
+            }
+
+            reflectionModifier = createReflectionModifier(patternList);
+
             reflection.addModifier(reflectionModifier);
         }
     }
 
-    private static BannerReflectionModifier createReflectionModifier() {
+    private static BannerReflectionModifier createReflectionModifier(List<? extends Pair<BannerPattern, DyeColor>> patternList) {
         return DistExecutor.runForDist(
-                () -> BannerReflectionModifierClient::new,
-                () -> BannerReflectionModifier::new
+                () -> () -> new BannerReflectionModifierClient(patternList),
+                () -> () -> new BannerReflectionModifier(patternList)
         );
     }
 
--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifierClient.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifierClient.java
@@ -3,12 +3,20 @@ package com.tomboshoven.minecraft.magicmirror.reflection.modifiers;
 import com.tomboshoven.minecraft.magicmirror.reflection.renderers.ReflectionRendererBase;
 import com.tomboshoven.minecraft.magicmirror.reflection.renderers.modifiers.BannerReflectionRendererModifier;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.item.DyeColor;
+import net.minecraft.tileentity.BannerPattern;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Collection;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class BannerReflectionModifierClient extends BannerReflectionModifier {
+    public BannerReflectionModifierClient(Collection<? extends Pair<BannerPattern, DyeColor>> patternList) {
+        super(patternList);
+    }
+
     @Override
     public ReflectionRendererBase apply(ReflectionRendererBase reflectionRenderer) {
         super.apply(reflectionRenderer);
