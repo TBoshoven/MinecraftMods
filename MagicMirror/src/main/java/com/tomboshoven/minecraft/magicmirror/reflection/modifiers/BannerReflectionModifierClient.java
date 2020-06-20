2a08d9e0c61f554ae81540748486ce591f2e1b75
Tom Boshoven <tomboshoven@gmail.com>
2020-06-20 13:50:23 -0400

    Add reflection renderer modifier.

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifierClient.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifierClient.java
@@ -1,5 +1,7 @@
 package com.tomboshoven.minecraft.magicmirror.reflection.modifiers;
 
+import com.tomboshoven.minecraft.magicmirror.reflection.renderers.ReflectionRendererBase;
+import com.tomboshoven.minecraft.magicmirror.reflection.renderers.modifiers.BannerReflectionRendererModifier;
 import mcp.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -7,4 +9,9 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class BannerReflectionModifierClient extends BannerReflectionModifier {
+    @Override
+    public ReflectionRendererBase apply(ReflectionRendererBase reflectionRenderer) {
+        super.apply(reflectionRenderer);
+        return new BannerReflectionRendererModifier(reflectionRenderer);
+    }
 }
