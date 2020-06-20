ddad7238cd0449bde177897f9cb1594f9e977abf
Tom Boshoven <tomboshoven@gmail.com>
2020-06-20 13:50:23 -0400

    Add reflection renderer modifier.

---
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/renderers/modifiers/BannerReflectionRendererModifier.java
@@ -0,0 +1,9 @@
+package com.tomboshoven.minecraft.magicmirror.reflection.renderers.modifiers;
+
+import com.tomboshoven.minecraft.magicmirror.reflection.renderers.ReflectionRendererBase;
+
+public class BannerReflectionRendererModifier extends ReflectionRendererModifier {
+    public BannerReflectionRendererModifier(ReflectionRendererBase baseRenderer) {
+        super(baseRenderer);
+    }
+}
