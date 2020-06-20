a058ffc60a5170c63427e9eb33abfe4be5e9257e
Tom Boshoven <tomboshoven@gmail.com>
2020-06-20 13:58:08 -0400

    Get the pattern list from the banner NBT and pass it to the reflection modifier.

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifier.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/modifiers/BannerReflectionModifier.java
@@ -1,9 +1,15 @@
 package com.tomboshoven.minecraft.magicmirror.reflection.modifiers;
 
+import com.google.common.collect.ImmutableList;
 import com.tomboshoven.minecraft.magicmirror.reflection.renderers.ReflectionRendererBase;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.item.DyeColor;
+import net.minecraft.tileentity.BannerPattern;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * A reflection modifier that changes the background of the reflection.
@@ -11,6 +17,12 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class BannerReflectionModifier extends ReflectionModifier {
+    ImmutableList<Pair<BannerPattern, DyeColor>> patternList;
+
+    public BannerReflectionModifier(Collection<? extends Pair<BannerPattern, DyeColor>> patternList) {
+        this.patternList = ImmutableList.copyOf(patternList);
+    }
+
     @Override
     public ReflectionRendererBase apply(ReflectionRendererBase reflectionRenderer) {
         // Nothing to do on the server side
