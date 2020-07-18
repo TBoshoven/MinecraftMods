15df576847461b964acd194dbcdb173d67cfad36
Tom Boshoven <tomboshoven@gmail.com>
2020-07-18 13:05:35 -0400

    Use ingredient suppliers instead of tags for doorknob recipes.

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/items/Items.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/items/Items.java
@@ -7,8 +7,8 @@ import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemTier;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.RegistryEvent.Register;
@@ -18,6 +18,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * Collection of all items in the mod.
@@ -39,14 +40,14 @@ public final class Items {
      * <p>
      * Make sure to add a translation key.
      *
-     * @param registry  The registry to which to add the doorknob item.
+     * @param registry    The registry to which to add the doorknob item.
      * @param typeName    The type name of the item. Keep this stable, since it is used in NBT data.
      * @param tier        The material this doorknob is made of
      * @param mainTexture The main texture of the doorknob
-     * @param recipeTag   The tag to use in recipes
+     * @param ingredient  The ingredient used to build the doorknob
      */
-    private static void addDoorknob(IForgeRegistry<? super Item> registry, String typeName, IItemTier tier, ResourceLocation mainTexture, Tag<Item> recipeTag) {
-        MagicDoorknobItem i = new MagicDoorknobItem(new Item.Properties().group(ItemGroup.TOOLS), typeName, tier, mainTexture, recipeTag);
+    private static void addDoorknob(IForgeRegistry<? super Item> registry, String typeName, IItemTier tier, ResourceLocation mainTexture, Supplier<Ingredient> ingredient) {
+        MagicDoorknobItem i = new MagicDoorknobItem(new Item.Properties().group(ItemGroup.TOOLS), typeName, tier, mainTexture, ingredient);
         i.setRegistryName(MagicDoorknobMod.MOD_ID, String.format("magic_doorknob_%s", typeName));
         registry.register(i);
         DOORKNOBS.put(typeName, i);
@@ -59,10 +60,10 @@ public final class Items {
      * @param typeName  The type name of the item. Keep this stable, since it is used in NBT data.
      * @param tier      The material this doorknob is made of
      * @param blockName The name of the block that provides the texture of the doorknob
-     * @param recipeTag The tag to use in recipes
+     * @param ingredient  The ingredient used to build the doorknob
      */
-    private static void addDoorknob(IForgeRegistry<? super Item> registry, String typeName, IItemTier tier, String blockName, Tag<Item> recipeTag) {
-        addDoorknob(registry, typeName, tier, new ResourceLocation("minecraft", String.format("block/%s", blockName)), recipeTag);
+    private static void addDoorknob(IForgeRegistry<? super Item> registry, String typeName, IItemTier tier, String blockName, Supplier<Ingredient> ingredient) {
+        addDoorknob(registry, typeName, tier, new ResourceLocation("minecraft", String.format("block/%s", blockName)), ingredient);
     }
 
     public static void register(IEventBus eventBus) {
@@ -73,10 +74,10 @@ public final class Items {
         IForgeRegistry<Item> registry = event.getRegistry();
 
         // Add all Vanilla tool materials
-        addDoorknob(registry, "wood", ItemTier.WOOD, "oak_planks", ItemTags.PLANKS);
-        addDoorknob(registry, "stone", ItemTier.STONE, "stone", Tags.Items.COBBLESTONE);
-        addDoorknob(registry, "iron", ItemTier.IRON, "iron_block", Tags.Items.INGOTS_IRON);
-        addDoorknob(registry, "gold", ItemTier.GOLD, "gold_block", Tags.Items.INGOTS_GOLD);
-        addDoorknob(registry, "diamond", ItemTier.DIAMOND, "diamond_block", Tags.Items.GEMS_DIAMOND);
+        addDoorknob(registry, "wood", ItemTier.WOOD, "oak_planks", () -> Ingredient.fromTag(ItemTags.PLANKS));
+        addDoorknob(registry, "stone", ItemTier.STONE, "stone", () -> Ingredient.fromTag(Tags.Items.COBBLESTONE));
+        addDoorknob(registry, "iron", ItemTier.IRON, "iron_block", () -> Ingredient.fromTag(Tags.Items.INGOTS_IRON));
+        addDoorknob(registry, "gold", ItemTier.GOLD, "gold_block", () -> Ingredient.fromTag(Tags.Items.INGOTS_GOLD));
+        addDoorknob(registry, "diamond", ItemTier.DIAMOND, "diamond_block", () -> Ingredient.fromTag(Tags.Items.GEMS_DIAMOND));
     }
 }
