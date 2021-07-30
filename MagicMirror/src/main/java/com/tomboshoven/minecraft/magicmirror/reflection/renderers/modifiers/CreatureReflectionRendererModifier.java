6f884e2734663a66c7620b6bad068dc0e26a3d6a
Tom Boshoven <tomboshoven@gmail.com>
2021-07-29 20:44:02 -0400

    Bunch of changes to the way we render mob reflections.

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/renderers/modifiers/CreatureReflectionRendererModifier.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/reflection/renderers/modifiers/CreatureReflectionRendererModifier.java
@@ -2,6 +2,7 @@ package com.tomboshoven.minecraft.magicmirror.reflection.renderers.modifiers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.tomboshoven.minecraft.magicmirror.reflection.renderers.ReflectionRendererBase;
+import com.tomboshoven.minecraft.magicmirror.renderers.Renderers;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ArmedModel;
@@ -10,11 +11,14 @@ import net.minecraft.client.model.HeadedModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.HumanoidModel.ArmPose;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.ArrowLayer;
 import net.minecraft.client.renderer.entity.layers.BeeStingerLayer;
@@ -51,7 +55,7 @@ public class CreatureReflectionRendererModifier extends ReflectionRendererModifi
      */
     public CreatureReflectionRendererModifier(ReflectionRendererBase baseRenderer) {
         super(baseRenderer);
-        replacementRenderer = new RenderOffModelPlayer<>(Minecraft.getInstance().getEntityRenderDispatcher(), new ModelSkeletonPlayer<>(), new ResourceLocation("textures/entity/skeleton/skeleton.png"));
+        replacementRenderer = Minecraft.getInstance().getEntityRenderDispatcher().renderers.get(Renderers.REFLECTION_ENTITY_TYPE);
     }
 
     @Override
@@ -60,131 +64,5 @@ public class CreatureReflectionRendererModifier extends ReflectionRendererModifi
         setRenderer(replacementRenderer);
         super.render(facing, partialTicks, renderTypeBuffer);
         setRenderer(originalRenderer);
-    }
-
-    /**
-     * A renderer to render a player using a different (biped) model.
-     */
-    @ParametersAreNonnullByDefault
-    @MethodsReturnNonnullByDefault
-    private static class RenderOffModelPlayer<T extends LivingEntity, M extends EntityModel<T>> extends LivingEntityRenderer<T, M> {
-        /**
-         * The location of the texture to use.
-         */
-        private final ResourceLocation textureLocation;
-
-        RenderOffModelPlayer(EntityRendererProvider.Context renderContext, M model, ResourceLocation textureLocation) {
-            super(renderContext, model, 0);
-
-            // Can't use explicit types here because there is no way to check whether the type M implements these
-            // interfaces or subclasses these classes due to type erasure and I don't want to work with too many
-            // subclasses.
-            if (model instanceof HeadedModel) {
-                //noinspection unchecked,rawtypes
-                addLayer(new CustomHeadLayer(this));
-            }
-            addLayer(new ElytraLayer<>(this));
-            if (model instanceof ArmedModel) {
-                //noinspection unchecked,rawtypes
-                addLayer(new ItemInHandLayer(this));
-            }
-            if (model instanceof HumanoidModel) {
-                //noinspection unchecked,rawtypes
-                addLayer(new HumanoidArmorLayer(this, new HumanoidModel<>(0.5F), new HumanoidModel<>(1.0F)));
-            }
-            if (model instanceof PlayerModel) {
-                //noinspection unchecked,rawtypes
-                addLayer(new ArrowLayer(this));
-                //noinspection unchecked,rawtypes
-                addLayer(new ParrotOnShoulderLayer(this));
-                //noinspection unchecked,rawtypes
-                addLayer(new SpinAttackEffectLayer(this));
-                //noinspection unchecked,rawtypes
-                addLayer(new BeeStingerLayer(this));
-            }
-            this.textureLocation = textureLocation;
-        }
-
-        @Override
-        public void render(T entityIn, float entityYaw, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn) {
-            setArmPoses(entityIn);
-            super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
-        }
-
-        /**
-         * Properly set the arm poses based on the player.
-         * This code is adapted from RenderPlayer.
-         *
-         * @param entity The entity to take the arm poses from.
-         */
-        private void setArmPoses(T entity) {
-            M model = getModel();
-
-            ItemStack[] handItems = {entity.getMainHandItem(), entity.getOffhandItem()};
-            ArmPose[] armPoses = {ArmPose.EMPTY, ArmPose.EMPTY};
-
-            for (int side = 0; side < 2; ++side) {
-                if (!handItems[side].isEmpty()) {
-                    armPoses[side] = ArmPose.ITEM;
-
-                    if (entity.getUseItemRemainingTicks() > 0) {
-                        UseAnim itemUseAction = handItems[side].getUseAnimation();
-
-                        if (itemUseAction == UseAnim.BLOCK) {
-                            armPoses[side] = ArmPose.BLOCK;
-                        } else if (itemUseAction == UseAnim.BOW) {
-                            armPoses[side] = ArmPose.BOW_AND_ARROW;
-                        }
-                    }
-                }
-            }
-
-            if (model instanceof HumanoidModel) {
-                HumanoidModel<?> bipedModel = (HumanoidModel<?>) model;
-                bipedModel.crouching = entity.isShiftKeyDown();
-                if (entity.getMainArm() == HumanoidArm.RIGHT) {
-                    bipedModel.rightArmPose = armPoses[0];
-                    bipedModel.leftArmPose = armPoses[1];
-                } else {
-                    bipedModel.rightArmPose = armPoses[1];
-                    bipedModel.leftArmPose = armPoses[0];
-                }
-            }
-        }
-
-        @Override
-        public ResourceLocation getTextureLocation(T entity) {
-            return textureLocation;
-        }
-    }
-
-    /**
-     * A player model that renders a skeleton.
-     * Skeletons don't use the default biped model, but the actual skeleton model requires a mob entity.
-     */
-    @ParametersAreNonnullByDefault
-    @MethodsReturnNonnullByDefault
-    private static class ModelSkeletonPlayer<T extends LivingEntity> extends HumanoidModel<T> {
-        @SuppressWarnings("AssignmentToSuperclassField")
-        ModelSkeletonPlayer() {
-            super(0, 0, 64, 32);
-
-            // Skeletons have slightly different models, so we just apply the same modifications as the skeleton model
-            // does.
-            rightArm = new ModelPart(this, 40, 16);
-            rightArm.addBox(-1.0F, -2.0F, -1.0F, 2, 12, 2, 0);
-            rightArm.setPos(-5.0F, 2.0F, 0.0F);
-            leftArm = new ModelPart(this, 40, 16);
-            leftArm.mirror = true;
-            leftArm.addBox(-1.0F, -2.0F, -1.0F, 2, 12, 2, 0);
-            leftArm.setPos(5.0F, 2.0F, 0.0F);
-            rightLeg = new ModelPart(this, 0, 16);
-            rightLeg.addBox(-1.0F, 0.0F, -1.0F, 2, 12, 2, 0);
-            rightLeg.setPos(-2.0F, 12.0F, 0.0F);
-            leftLeg = new ModelPart(this, 0, 16);
-            leftLeg.mirror = true;
-            leftLeg.addBox(-1.0F, 0.0F, -1.0F, 2, 12, 2, 0);
-            leftLeg.setPos(2.0F, 12.0F, 0.0F);
-        }
     }
 }
--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/renderers/Renderers.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/renderers/Renderers.java
@@ -2,7 +2,15 @@ package com.tomboshoven.minecraft.magicmirror.renderers;
 
 import com.tomboshoven.minecraft.magicmirror.blocks.tileentities.TileEntities;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -17,6 +25,13 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public final class Renderers {
+    public static EntityType<LivingEntity> REFLECTION_ENTITY_TYPE = EntityType.Builder.<LivingEntity>createNothing(MobCategory.MISC).build("off_model_player");
+
+    static EntityRendererProvider<LivingEntity> REFLECTION_ENTITY_RENDERER_PROVIDER = OffModelPlayerRenderer.createProvider(
+            context -> new HumanoidModel<>(context.bakeLayer(ModelLayers.SKELETON)),
+            new ResourceLocation("textures/entity/skeleton/skeleton.png")
+    );
+
     private Renderers() {
     }
 
@@ -27,5 +42,7 @@ public final class Renderers {
     private static void registerRenderers(ModelRegistryEvent event) {
         BlockEntityRenderers.register(TileEntities.MAGIC_MIRROR_CORE.get(), TileEntityMagicMirrorRenderer::new);
         BlockEntityRenderers.register(TileEntities.MAGIC_MIRROR_PART.get(), TileEntityMagicMirrorRenderer::new);
+
+        EntityRenderers.register(REFLECTION_ENTITY_TYPE, REFLECTION_ENTITY_RENDERER_PROVIDER);
     }
 }
