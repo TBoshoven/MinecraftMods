9af04a85129b8a1cb9f74eedb34ec145c80609ec
Tom Boshoven <tomboshoven@gmail.com>
2023-11-26 12:36:55 -0500

    Solve this a bit more cleanly and deal with the registry default.

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/modifiers/CreatureMagicMirrorModifier.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/modifiers/CreatureMagicMirrorModifier.java
@@ -25,7 +25,29 @@ public class CreatureMagicMirrorModifier extends MagicMirrorModifier {
      */
     private static boolean isSupportedSkull(ItemStack item) {
         // Only support skeleton skulls for now
-        return item.getItem() == Items.SKELETON_SKULL && Items.SKELETON_SKULL.getDamage(item) == 0;
+        return item.getItem() == Items.SKELETON_SKULL;
+    }
+
+    /**
+     * Check whether the given entity type is supported by the mirror.
+     * This can be used as a safety check to prevent crashes upon bad NBT data.
+     *
+     * @param entityType The entity type to check.
+     * @return whether the provided entity type is supported by the modifier.
+     */
+    public static boolean isSupportedEntityType(EntityType<?> entityType) {
+        // Only skeletons are supported for now
+        return entityType == EntityType.SKELETON;
+    }
+
+    /**
+     * Get the default entity type to use when we have no information.
+     * This can be used for recovery upon bad data.
+     *
+     * @return the entity type to use when we don't have the required information.
+     */
+    public static EntityType<?> getDefaultEntityType() {
+        return EntityType.SKELETON;
     }
 
     @Override
@@ -45,6 +67,8 @@ public class CreatureMagicMirrorModifier extends MagicMirrorModifier {
 
     @Override
     MagicMirrorBlockEntityModifier createBlockEntityModifier(ItemStack usedItem) {
-        return new CreatureMagicMirrorBlockEntityModifier(this, usedItem.split(1), EntityType.SKELETON);
+        // Only one entity type is supported for now
+        EntityType<?> entityType = getDefaultEntityType();
+        return new CreatureMagicMirrorBlockEntityModifier(this, usedItem.split(1), entityType);
     }
 }
