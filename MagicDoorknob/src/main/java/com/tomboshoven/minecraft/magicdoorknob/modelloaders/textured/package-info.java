eedae2388febd0fed4a07ffd2907f368512624fa
Tom Boshoven <tomboshoven@gmail.com>
2023-12-31 19:15:45 +0100

    Move client code to its own namespace.

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/modelloaders/textured/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package com.tomboshoven.minecraft.magicdoorknob.modelloaders.textured;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
---
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/client/reflection/modifiers/CreatureReflectionModifier.java
@@ -0,0 +1,19 @@
+package com.tomboshoven.minecraft.magicmirror.client.reflection.modifiers;
+
+import com.tomboshoven.minecraft.magicmirror.blocks.entities.modifiers.CreatureMagicMirrorBlockEntityModifier;
+import com.tomboshoven.minecraft.magicmirror.blocks.entities.modifiers.MagicMirrorBlockEntityModifier;
+import com.tomboshoven.minecraft.magicmirror.client.reflection.renderers.ReflectionRendererBase;
+import com.tomboshoven.minecraft.magicmirror.client.reflection.renderers.modifiers.CreatureReflectionRendererModifier;
+
+/**
+ * A reflection modifier that changes the armor that the entity used to represent the character.
+ */
+public class CreatureReflectionModifier extends ReflectionModifier {
+    @Override
+    public ReflectionRendererBase apply(MagicMirrorBlockEntityModifier modifier, ReflectionRendererBase reflectionRenderer) {
+        if (modifier instanceof CreatureMagicMirrorBlockEntityModifier creatureModifier) {
+            return new CreatureReflectionRendererModifier(reflectionRenderer, creatureModifier.getEntityType());
+        }
+        return reflectionRenderer;
+    }
+}
