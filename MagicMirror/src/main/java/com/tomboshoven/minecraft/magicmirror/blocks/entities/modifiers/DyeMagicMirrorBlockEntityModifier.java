fd77224ff800adb476ffb95839cd9530190a31e8
Tom Boshoven <tomboshoven@gmail.com>
2024-01-01 17:15:14 +0100

    Add dye modifier to the Magic Mirror
    Squashed commit of the following:
    
    commit 6520bedeb6d9f65afd2de56f457101963cc7203d
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Mon Jan 1 17:01:20 2024 +0100
    
        Missing comment
    
    commit 2fa32259c0defb93490b628821ca02dd9c537cb7
    Merge: eec464b 5365db1
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Mon Jan 1 16:54:07 2024 +0100
    
        Merge remote-tracking branch 'origin/1.20.1' into 1.20.1-dye-modifier
    
    commit eec464bb9a783e6431b8af19905d58383fddad9e
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Sun Nov 26 19:55:48 2023 -0500
    
        Apply changes from modifier rework
    
    commit 15a001ffb11395400a9c7a11735770a2f8d106a7
    Merge: 15dacda 594edb8
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Sun Nov 26 18:56:41 2023 -0500
    
        Merge branch '1.20.1' into 1.20.1-dye-modifier
    
    commit 15dacda487ecfe7a3a84beb8a0a983da0f79bb31
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Sat Nov 25 11:54:57 2023 -0500
    
        Fix up imports
    
    commit 673f0fb7987d821815a5bfc3db13c8bfecb354d6
    Author: Tom Boshoven <tomboshoven@gmail.com>
    Date:   Sat Nov 25 01:43:33 2023 -0500
    
        Add a dye modifier to the magic mirror

---
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/entities/modifiers/DyeMagicMirrorBlockEntityModifier.java
@@ -0,0 +1,47 @@
+package com.tomboshoven.minecraft.magicmirror.blocks.entities.modifiers;
+
+import com.tomboshoven.minecraft.magicmirror.blocks.entities.MagicMirrorCoreBlockEntity;
+import com.tomboshoven.minecraft.magicmirror.blocks.modifiers.MagicMirrorModifier;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DyeItem;
+import net.minecraft.world.item.ItemStack;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class DyeMagicMirrorBlockEntityModifier extends ItemBasedMagicMirrorBlockEntityModifier {
+    /**
+     * @param modifier The modifier that applied this object to the block entity.
+     */
+    public DyeMagicMirrorBlockEntityModifier(MagicMirrorModifier modifier, ItemStack item) {
+        super(modifier, item);
+    }
+
+    public DyeMagicMirrorBlockEntityModifier(MagicMirrorModifier modifier, CompoundTag nbt) {
+        super(modifier, nbt);
+    }
+
+    @Override
+    public boolean tryPlayerActivate(MagicMirrorCoreBlockEntity blockEntity, Player playerIn, InteractionHand hand) {
+        // No behavior right now.
+        return false;
+    }
+
+    /**
+     * @return the color of the dye that was used.
+     */
+    public float[] getColor() {
+        float[] color;
+        if (item.getItem() instanceof DyeItem dyeItem) {
+            float[] textureDiffuseColors = dyeItem.getDyeColor().getTextureDiffuseColors();
+            color = new float[]{textureDiffuseColors[0], textureDiffuseColors[1], textureDiffuseColors[2], 1f};
+        } else {
+            color = new float[]{1f, 1f, 1f, 1f};
+        }
+        return color;
+    }
+}
