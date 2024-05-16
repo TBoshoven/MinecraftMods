95082b8423b165f9d51ee442ad9b4136de3d74fc
Tom Boshoven <tomboshoven@gmail.com>
2024-05-16 18:11:51 -0400

    Reorder annotations to be in line with later versions

--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/entities/modifiers/package-info.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/blocks/entities/modifiers/package-info.java
@@ -1,5 +1,5 @@
-@ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 package com.tomboshoven.minecraft.magicmirror.blocks.entities.modifiers;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
--- MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/data/package-info.java
+++ MagicMirror/src/main/java/com/tomboshoven/minecraft/magicmirror/data/package-info.java
@@ -1,5 +1,5 @@
-@ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 package com.tomboshoven.minecraft.magicmirror.data;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
