601a2e39417de578233afa77e80e03fd121da3a3
Tom Boshoven <tomboshoven@gmail.com>
2025-08-28 21:30:58 -0400

    Proper particle texture handling

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/blocks/entities/MagicDoorwayPartBaseBlockEntity.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/blocks/entities/MagicDoorwayPartBaseBlockEntity.java
@@ -45,6 +45,11 @@ public abstract class MagicDoorwayPartBaseBlockEntity extends BlockEntity {
      */
     private static final ModelTextureProperty TEXTURE_HIGHLIGHT = ModelTextureProperty.get(new ResourceLocation(PROPERTY_NAMESPACE, "texture_highlight"));
 
+    /**
+     * The particle texture of the doorway (based on base block particle texture).
+     */
+    public static final ModelTextureProperty TEXTURE_PARTICLE = ModelTextureProperty.get(new ResourceLocation(PROPERTY_NAMESPACE, "texture_particle"));
+
     // Material to use when no proper material can be found, such as with air.
     @SuppressWarnings("deprecation")
     private static final Material EMPTY_MATERIAL = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(MOD_ID, "block/empty"));
@@ -112,6 +117,7 @@ public abstract class MagicDoorwayPartBaseBlockEntity extends BlockEntity {
 
         // Fallback chain is block texture -> empty
         TextureSourceReference fallbackReference = new TextureSourceReference.MaterialTextureSource(EMPTY_MATERIAL);
+        TextureSourceReference particleTextureSourceReference = new TextureSourceReference.BlockParticle(baseBlockState);
         TextureSourceReference blockTextureSourceReference = new TextureSourceReference.BlockLookup(level, blockPos, baseBlockState, fallbackReference);
 
         TextureSourceReference doorknobTextureSourceReference;
@@ -126,6 +132,7 @@ public abstract class MagicDoorwayPartBaseBlockEntity extends BlockEntity {
         return ModelData.builder()
                 .with(TEXTURE_MAIN, blockTextureSourceReference)
                 .with(TEXTURE_HIGHLIGHT, doorknobTextureSourceReference)
+                .with(TEXTURE_PARTICLE, particleTextureSourceReference)
                 .build();
     }
 
