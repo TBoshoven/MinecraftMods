d6d796a8212e2b7a1431aa867bc9004c08cbcb2a
Tom Boshoven <tomboshoven@gmail.com>
2026-01-03 16:28:08 +0100

    Implement suffocation prevention

--- MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/blocks/entities/MagicDoorwayBlockEntity.java
+++ MagicDoorknob/src/main/java/com/tomboshoven/minecraft/magicdoorknob/blocks/entities/MagicDoorwayBlockEntity.java
@@ -1,13 +1,55 @@
 package com.tomboshoven.minecraft.magicdoorknob.blocks.entities;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
  * Block entity for the magic doorway parts.
  */
 public class MagicDoorwayBlockEntity extends MagicDoorwayPartBaseBlockEntity {
+    // Whether the door is currently closing
+    // Used for preventing suffocation because someone closed the door
+    private boolean isClosing = false;
+
     public MagicDoorwayBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntities.MAGIC_DOORWAY.get(), pos, state);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        Level level = getLevel();
+        // Schedule the block tick in case we're closing
+        if (isClosing && level != null) {
+            level.scheduleTick(getBlockPos(), getBlockState().getBlock(), 10);
+        }
+    }
+
+    @Override
+    protected void saveInternal(CompoundTag compound) {
+        super.saveInternal(compound);
+        compound.putBoolean("isClosing", isClosing);
+    }
+
+    @Override
+    protected void loadInternal(CompoundTag compound) {
+        super.loadInternal(compound);
+        isClosing = compound.getBoolean("isClosing");
+    }
+
+    /**
+     * @return Whether the doorway is currently closing.
+     */
+    public boolean isClosing() {
+        return this.isClosing;
+    }
+
+    /**
+     * Mark the doorway as closing, causing it to close when there are no more living entities in it.
+     */
+    public void setClosing() {
+        this.isClosing = true;
     }
 }
